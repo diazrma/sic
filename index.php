302b90587f89acf9c1f9ea4a8ebd1622e9285070
Rodrigo Cardoso <diazrma@gmail.com>
2020-05-15 12:19:19 -0300

    index

--- index.php
+++ index.php
@@ -11,9 +11,11 @@
 <body>
     <div class="vh-100 w-100 d-flex justify-content-center align-items-center">
         <div>
-            <a href="views/cadastro.php" class="btn btn-primary ml-10">Cadastrar</a>
-            <a href="views/listar.php" class="btn btn-secondary ml-10">Listar</a>
-            <a href="views/logs.php" class="btn btn-warning ml-10">Logs</a>
+            <h1>SIC</h1>
+            <h3>Sistema de cadastro de clientes</h3>
+            <a href="views/cadastro.php" class="btn btn-primary ml-10">Cadastrar Cliente</a>
+            <a href="views/listar.php" class="btn btn-secondary ml-10">Listar Clientes</a>
+            <a href="views/logs.php" class="btn btn-warning ml-10">Logs do sistema</a>
         </div>
     </div>
 
