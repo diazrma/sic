2bfac80b14d6187e296aa72cff3cfa3f7b2ac31f
Rodrigo Cardoso <diazrma@gmail.com>
2020-05-15 12:07:02 -0300

    index

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sitema de cadastro de clientes</title>
 </head>
+<?php require_once('views/head.php'); ?>
+
 <body>
+    <div class="vh-100 w-100 d-flex justify-content-center align-items-center">
+        <div>
+            <a href="views/cadastro.php" class="btn btn-primary ml-10">Cadastrar</a>
+            <a href="views/listar.php" class="btn btn-secondary ml-10">Listar</a>
+            <a href="views/logs.php" class="btn btn-warning ml-10">Logs</a>
+        </div>
+    </div>
 
-<a href="views/cadastro.php">Cadastrar</a>
-<a href="views/listar.php">Listar</a>
-
-    
 </body>
+
 </html>
